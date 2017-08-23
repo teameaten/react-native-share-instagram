c7b120ac262b24a1ee28506da9fa793b29308105
micabe <mickael.abensur@launchmetrics.com>
2017-08-23 17:38:13 +0200

    fix mistake

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 import { NativeModules } from 'react-native';
 
-export default RNReactNativeSharingWinstagram.NativeModules || {};
+export default NativeModules.RNReactNativeSharingWinstagram || {};
